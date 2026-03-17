f91e95380485f4ab60cea5cac897b6e0d1ceb55d
VictorRaupp <vp93591@gmail.com>
2026-03-17 14:02:02 -0300

    adicionados metodos de envio de email

---
+++ PROJETOS/aprendendo-spring-main/aprendendo-spring-main/src/main/java/com/victor/aprendendospring/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.victor.aprendendospring.infrastructure.repository;
+
+
+import com.victor.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
